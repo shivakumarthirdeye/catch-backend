38a513cb19de5f3871757b8988f2962dc4d47542
Yogesh <yogesh.kumar.rr5@gmail.com>
2023-01-13 15:17:20 +0530

    fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'catch_api',
-      script: './src/main.js',
+      script: '/build/src/main.js',
       exec_mode: 'cluster',
       instances: 1,
       env: {
