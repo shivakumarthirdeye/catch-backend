e5ce142d2afaae10095eceff28d39fc4f1acf7fa
Yogesh <yogesh.kumar.rr5@gmail.com>
2023-01-13 12:38:15 +0530

    fixes

--- build/src/individual/authentication/authToken/tokens.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-// This function generate access token
-function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10d" });
-}
-module.exports = generateAccessToken;
-//# sourceMappingURL=tokens.js.map
